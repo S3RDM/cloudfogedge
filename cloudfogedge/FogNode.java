7de1ce05d116f6b8178c52d138785a6e61c188f3
S3RDM <legochecho@gmail.com>
2026-04-07 21:28:24 +0200

    versión 1

--- cloudfogedge/FogNode.java
+++ cloudfogedge/FogNode.java
@@ -2,17 +2,27 @@ package cloudfogedge;
 
 public class FogNode {
     private CloudServer cloudServer;
+    private int contadorTemp;
+    private final int limite = 20;
 
     public FogNode(CloudServer cloudServer){
         this.cloudServer = cloudServer;
     }
 
-    public void procesarDatos(SensorData dato){
+    public boolean procesarDatos(SensorData dato){
         System.out.println("FogNode recibe: " + dato);
 
         if(dato.getTemperatura() > 30){
+            contadorTemp++;
             System.out.println("Temperatura alta detectada.");
         }
         cloudServer.saveData(dato);
+        
+        if (contadorTemp >= limite) {
+            System.out.println("\n--- Limite de 20 temperaturas alcanzado ---");
+            return false;
+        }
+
+        return true;
     }
 }
